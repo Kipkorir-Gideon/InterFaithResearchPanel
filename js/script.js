d019aeeee40b43dd73435b7199fc52faea495c68
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-08-21 10:57:59 +0300

    Add a event toggle functionality.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 30)
 })
+
+
+
+const events = document.querySelectorAll('.event');
+
+events.forEach(event => {
+    event.addEventListener('click', () => {
+        event.classList.toggle('open')
+    })
+})
